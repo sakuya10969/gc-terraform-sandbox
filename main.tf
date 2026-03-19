7648d375aa5217772f8587174d4a0a184f967290
sakuya10969 <sakuya.f20010618@gmail.com>
2026-03-19 13:44:48 +0900

    fix

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "google_project_service" "apis" {
   project                    = var.project_id
   service                    = each.value
   disable_dependent_services = false
-  disable_on_destroy         = true
+  disable_on_destroy         = false
 }
 
 # -------------------------------------------------------------------
